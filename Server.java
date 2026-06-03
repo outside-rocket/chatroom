9ad5e837f1805e407b7927fcfda3e8dd3fd37056
outside-rocket <varunkrishnav008@gmail.com>
2026-06-03 16:05:56 +0530

    server java file partially written at alternate destination using an android device

---
+++ Server.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Server{
+	ArrayList<ClientHandler> clients = new ArrayList();
+	public static void main(String[] args) throws Exception{
+		ServerSocket serverSocket = new ServerSocket(5000);
+		System.out.println(" server running ");
+		while(true){
+			Socket socket = serverSocket.accet();
+			System.out.println("New client connected");
+			ClientHandler clientHandler = new ClientHandler(socket);
+			clients.add(clientHandler);
+			Thread thread = new Thread(clientHandler);
+			thread.start();
+
+		}
+	}
+	public static void broadcast(String sender, ClientHandler sender){
+		
+	}
+}
