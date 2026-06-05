f1de0939ab31cd8677714482b017446fff5167ca
维维克 <104138251+outside-rocket@users.noreply.github.com>
2026-06-05 19:47:12 +0530

    server file completed, added the remove, broadcast features

--- server.java
+++ server.java
@@ -1,22 +1,34 @@
-import java.net.io.*;
 import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
 
-public class server{
+public class Server{
+	ArrayList<ClientHandler> clients = new ArrayList();
+	public static void main(String[] args) throws Exception{
+		ServerSocket serverSocket = new ServerSocket(5000);
+		System.out.println(" server running ");
+		while(true){
+			Socket socket = serverSocket.accet();
+			System.out.println("New client connected");
+			ClientHandler clientHandler = new ClientHandler(socket);
+			clients.add(clientHandler);
+			Thread thread = new Thread(clientHandler);
+			thread.start();
 
-  static ArrayList<ClientHandler> clients = new ArrayLIst<>();
-  public static void main(String[] args) throw Exception{
+		}
+	}
+	public static void broadcast(String message, ClientHandler sender){
+    for(ClientHandler client: clients){
 
-    ServerSocket serversocket = new ServerSocket(5000);
-    System.out.println("Server started! ");
-    while(true){
-
-      Socket socket = serverSocket.accept();
-      System.out.println("new client cpnnected! ");
-      ClientHandler clienthandler = new ClientHandler(socket)
+      if(client != sender){
+        client.sendMessage(message);
+      }
       
     }
+	}
+  public static void removeClient(ClientHandler client){
+
+    clients.remove(client);
     
   }
-  
 }
